372f01ceab939df057c153b94d6e5bd15ffdfa7f
Garrett Schroath <gtschroath@gmail.com>
2021-03-06 14:24:23 -0600

    excercise 1 ec 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,22 @@
 
 import * as React from 'react'
 
-const countReducer = (state, newState) => {
-  if (typeof newState === 'function') {
-    return ({...state, ...newState(state)})
+const countReducer = (state, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return { count: state.count + action.step }
+    default:
+      throw new Error(`Unsupported action type ${action.type}`)
   }
-  return ({ ...state, ...newState })
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {count: initialCount})
-
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
-  return <button onClick={increment}>{state.count}</button>
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  return <button onClick={increment}>{count}</button>
 }
 
 function App() {
